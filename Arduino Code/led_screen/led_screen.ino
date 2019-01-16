64e6351a614ce6bee639245d5dc8746f24a41010
Thomas van Opstal <opstal.thomas@gmail.com>
2019-01-16 02:44:09 +0100

    code finished for testing.

--- Arduino Code/led_screen/led_screen.ino
+++ Arduino Code/led_screen/led_screen.ino
@@ -8,81 +8,176 @@
 #define PIN 6
 #define X 20
 #define Y 20
-#define Brightness 10
+#define Brightness 5
 
 // Color definitions
-#define BLACK    0x0000
 #define BLUE     0x001F
 #define RED      0xF800
 #define GREEN    0x07E0
-#define CYAN     0x07FF
-#define MAGENTA  0xF81F
-#define YELLOW   0xFFE0 
+#define YELLOW   0xFFE0
 #define WHITE    0xFFFF
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(X, Y, PIN,
-  NEO_MATRIX_TOP      + NEO_MATRIX_LEFT +
-  NEO_MATRIX_ROWS     + NEO_MATRIX_ZIGZAG,
-  NEO_GRB             + NEO_KHZ800);
+//Input
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
 
-//Level Data
-const uint8_t levelOne[] PROGMEM= {
-  0xff,0xff,0xf0,
-  0x80,0x0,0x10,
-  0xff,0x3f,0xd0,
-  0xff,0xff,0xd0,
-  0x80,0xc0,0x50,
-  0xbc,0xc0,0x50,
-  0xbf,0xc0,0x50,
-  0x80,0x0,0x10,
-  0xbf,0xc0,0x70,
-  0x80,0x40,0x70,
-  0xff,0x40,0x70,
-  0x81,0x40,0x70,
-  0xbd,0x7f,0xf0,
-  0x9d,0x7f,0xd0,
-  0x9c,0x0,0x10,
-  0x9f,0xf9,0xf0,
-  0xbf,0xff,0x10,
-  0x80,0x0,0x10,
-  0xff,0xff,0x10,
-  0xff,0xff,0xf0
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(X, Y, PIN,
+                            NEO_MATRIX_TOP      + NEO_MATRIX_LEFT +
+                            NEO_MATRIX_ROWS     + NEO_MATRIX_ZIGZAG,
+                            NEO_GRB             + NEO_KHZ800);
+
+const int levelOneArr[20][20] = {
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+  {1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+  {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+  {1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+  {1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+  {1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-  
+int playerHorizontalPosition = 1;
+int playerVerticalPosition = 1;
+
+//[0]horizontal - [1]vertical - [3]dir (0) = vert - (1) = hor
+int enemies[5][3] = {
+  {18, 1, 1},
+  {15, 4, 0},
+  {12, 11, 0},
+  {18, 14, 1},
+  {1, 11, 0},
+};
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+const long interval = 500;
+
 void setup() {
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(Brightness);
 
+  pinMode(UP, INPUT);
+  pinMode(DOWN, INPUT);
+  pinMode(LEFT, INPUT);
+  pinMode(RIGHT, INPUT);
 }
 
 void loop() {
-    gameController();
-    matrix.show();
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    UpdateEnemies();
+  }
+
+  MovePlayer();
+  PlayerCollisionDetection();
+  DrawPlayer();
+  DrawEnemies();
+  DrawLevelOne();
+  matrix.show();
 }
 
-void gameController(){
-    drawLevelOne();
-    drawObjectsOne();
-    drawPlayerOne();
+void UpdateEnemies() {
+  for (int i = 0; i < 5; i++) {
+    if (enemies[i][3] == 1) {
+      //Horizontal
+      if (enemies[i][2] == 1) {
+        //right
+        if (levelOneArr[enemies[i][0] + 1][enemies[i][1]] != 1) {
+          enemies[i][0] = enemies[i][0] + 1;
+        } else {
+          enemies[i][2] = -1;
+        }
+      } else {
+        //left
+        if (levelOneArr[enemies[i][0] - 1][enemies[i][1]] != 1) {
+          enemies[i][0] = enemies[i][0] - 1;
+        } else {
+          enemies[i][2] = 1;
+        }
+      }
+    } else {
+      //Vertical
+      if (enemies[i][2] == 1) {
+        //down
+        if (levelOneArr[enemies[i][0]][enemies[i][1] + 1] != 1) {
+          enemies[i][1] = enemies[i][1] + 1;
+        } else {
+          enemies[i][2] = -1;
+        }
+      } else {
+        //up
+        if (levelOneArr[enemies[i][0]][enemies[i][1] - 1] != 1) {
+          enemies[i][1] = enemies[i][1] - 1;
+        } else {
+          enemies[i][2] = 1;
+        }
+      }
+    }
+  }
+}
+
+void PlayerCollisionDetection() {
+  for (int i = 0; i < 5; i++) {
+    if (playerHorizontalPosition == enemies[i][0] && playerVerticalPosition == enemies[i][1])
+      playerHorizontalPosition = 1;
+    playerVerticalPosition = 1;
+  }
+}
+
+void MovePlayer() {
+  if (UP) {
+    if (levelOneArr[playerHorizontalPosition][playerVerticalPosition - 1] != 1)
+      playerVerticalPosition = playerVerticalPosition - 1;
   }
 
-void drawPlayerOne(){
-    matrix.drawPixel(1,1,WHITE);
+  if (DOWN) {
+    if (levelOneArr[playerHorizontalPosition][playerVerticalPosition + 1] != 1)
+      playerVerticalPosition = playerVerticalPosition + 1;
   }
-void drawLevelOne(){
-    matrix.drawBitmap(0,0,levelOne,20,20,GREEN);
+
+  if (RIGHT) {
+    if (levelOneArr[playerHorizontalPosition + 1][playerVerticalPosition] != 1)
+      playerVerticalPosition = playerVerticalPosition + 1;
   }
-void drawObjectsOne(){
-//Keys
-    matrix.drawPixel(7,5,YELLOW);
-    matrix.drawPixel(18,13,YELLOW);
-//Door
-    matrix.fillRect(17,16,2,3,RED);
+
+  if (LEFT) {
+    if (levelOneArr[playerHorizontalPosition - 1][playerVerticalPosition] != 1)
+      playerVerticalPosition = playerVerticalPosition - 1;
   }
-void drawEnemysOne(){}
+}
 
+void DrawPlayer() {
+  matrix.drawPixel(playerHorizontalPosition, playerVerticalPosition, WHITE);
+}
 
+void DrawEnemies() {
+  for (int i = 0; i < 5; i++) {
+    matrix.drawPixel(enemies[i][0], enemies[i][1], RED);
+  }
+}
 
-
+void DrawLevelOne() {
+  for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 20; j++) {
+      if (levelOneArr[i][j] == 1) {
+        matrix.drawPixel(i, j, GREEN);
+      }
+    }
+  }
+}
