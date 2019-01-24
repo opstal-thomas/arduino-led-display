82a5edf0c05a533ac812ff093b2826c95c7f2673
Thomas van Opstal <opstal.thomas@gmail.com>
2019-01-24 01:39:49 +0100

    proper code

--- Arduino Code/led_screen/led_screen.ino
+++ Arduino Code/led_screen/led_screen.ino
@@ -47,72 +47,23 @@ const int levelOneData[20][20] PROGMEM = {
   {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-//Level One Bitmap
-const PROGMEM uint8_t levelOne[] = {
-  0xff, 0xff, 0xf0,
-  0x80, 0x0, 0x10,
-  0xff, 0x3f, 0xd0,
-  0xff, 0xff, 0xd0,
-  0x80, 0xc0, 0x50,
-  0xbc, 0xc0, 0x50,
-  0xbf, 0xc0, 0x50,
-  0x80, 0x0, 0x10,
-  0xbf, 0xc0, 0x70,
-  0x80, 0x40, 0x70,
-  0xff, 0x40, 0x70,
-  0x81, 0x40, 0x70,
-  0xbd, 0x7f, 0xf0,
-  0x9d, 0x7f, 0xd0,
-  0x9c, 0x0, 0x10,
-  0x9f, 0xf9, 0xf0,
-  0xbf, 0xff, 0x10,
-  0x80, 0x0, 0x10,
-  0xff, 0xff, 0x10,
-  0xff, 0xff, 0xf0
-};
-
 const int keys[2][2] = {
-  {7, 5},
+  {8, 5},
   {18, 13},
 };
 
 int playerHorizontalPosition = 1;
 int playerVerticalPosition = 1;
 
-int enemyOneHorizontalPosition = 18;
-int enemyOneDir = -1;
-const int enemyOneStepCount = 15;
-int enemyOneCurrentStepCount = 0;
-
-int enemyTwoVerticalPosition = 4;
-int enemyTwoDir = 1;
-const int enemyTwoStepCount = 7;
-int enemyTwoCurrentStepCount = 0;
-
-int enemyThreeVerticalPosition = 11;
-int enemyThreeDir = -1;
-const int enemyThreeStepCount = 7;
-int enemyThreeCurrentStepCount = 0;
-
-int enemyFourVerticalPosition = 11;
-int enemyFourDir = 1;
-const int enemyFourStepCount = 6;
-int enemyFourCurrentStepCount = 0;
-
-int enemyFiveHorizontalPosition = 18;
-int enemyFiveDir = -1;
-const int enemyFiveStepCount = 9;
-int enemyFiveCurrentStepCount = 0;
-
 bool keyOneCollected = false;
 bool keyTwoCollected = false;
 
-int enemies[5][2] = {
-  {18, 1},
-  {15, 4},
-  {12, 11},
-  {18, 14},
-  {1, 11},
+int enemyData[5][6] = {
+  {18, 1, 1, -1, 15, 0},
+  {15, 4, -1, 1, 7, 0},
+  {12, 11, -1, -1, 7, 0},
+  {18, 14, 1, -1, 9, 0},
+  {1, 11, -1, 1, 6, 0},
 };
 
 void setup() {
@@ -130,26 +81,83 @@ void setup() {
 void loop() {
   matrix.fillScreen(0);
 
-  UpdateEnemys();
   PlayerCollisionDetection();
+  UpdateEnemies();
   MovePlayer();
 
-  DrawPlayer();
-  DrawLevelOne();
-  DrawKeys();
   DrawEnemies();
+  DrawPlayer();
+  DrawLevel();
+  DrawKeys();
 
   if (keyOneCollected && keyTwoCollected)
     DrawDoor();
 
   matrix.show();
-
   delay(100);
+}
+
+void UpdateEnemies() {
+  for (int i = 0; i < 5; i++) {
+    if (enemyData[i][2] == 1) {
+      //Horizontal
+      if (enemyData[i][3] == 1) {
+        //Right
+        if (enemyData[i][5] != enemyData[i][4]) {
+          enemyData[i][0] = enemyData[i][0] + 1;
+          enemyData[i][5] = enemyData[i][5] + 1;
+        } else {
+          enemyData[i][0] = enemyData[i][0] - 1;
+          enemyData[i][3] = -1;
+          enemyData[i][5] = 1;
+        }
+      } else {
+        //Left
+        if (enemyData[i][5] != enemyData[i][4]) {
+          enemyData[i][0] = enemyData[i][0] - 1;
+          enemyData[i][5] = enemyData[i][5] + 1;
+        } else {
+          enemyData[i][0] = enemyData[i][0] + 1;
+          enemyData[i][3] = 1;
+          enemyData[i][5] = 1;
+        }
+      }
+    } else {
+      //Vertical
+      if (enemyData[i][3] == 1) {
+        //Up
+        if (enemyData[i][5] != enemyData[i][4]) {
+          enemyData[i][1] = enemyData[i][1] + 1;
+          enemyData[i][5] = enemyData[i][5] + 1;
+        } else {
+          enemyData[i][1] = enemyData[i][1] - 1;
+          enemyData[i][3] = -1;
+          enemyData[i][5] = 1;
+        }
+      } else {
+        //Down
+        if (enemyData[i][5] != enemyData[i][4]) {
+          enemyData[i][1] = enemyData[i][1] - 1;
+          enemyData[i][5] = enemyData[i][5] + 1;
+        } else {
+          enemyData[i][1] = enemyData[i][1] + 1;
+          enemyData[i][3] = 1;
+          enemyData[i][5] = 1;
+        }
+      }
+    }
+  }
+}
+
+void DrawEnemies() {
+  for (int i = 0; i < 5; i++) {
+    matrix.drawPixel(enemyData[i][0], enemyData[i][1], RED);
+  }
 }
 
 void PlayerCollisionDetection() {
   for (int i = 0; i < 5; i++) {
-    if (playerHorizontalPosition == enemies[i][0] && playerVerticalPosition == enemies[i][1]) {
+    if (playerHorizontalPosition == enemyData[i][0] && playerVerticalPosition == enemyData[i][1]) {
       playerHorizontalPosition = 1;
       playerVerticalPosition = 1;
     }
@@ -162,7 +170,7 @@ void PlayerCollisionDetection() {
     keyTwoCollected = true;
 
   if (keyOneCollected && keyTwoCollected) {
-    if (playerHorizontalPosition == 18 && playerVerticalPosition == 18) {
+    if ((playerHorizontalPosition == 17 && playerVerticalPosition == 18) || (playerHorizontalPosition == 17 && playerVerticalPosition == 17) || (playerHorizontalPosition == 17 && playerVerticalPosition == 16)) {
       wdt_enable(WDTO_15MS);
 
       for (;;) {
@@ -208,17 +216,9 @@ void DrawPlayer() {
   matrix.drawPixel(playerHorizontalPosition, playerVerticalPosition, WHITE);
 }
 
-void DrawEnemies() {
-  matrix.drawPixel(enemyOneHorizontalPosition, 1, RED);
-  matrix.drawPixel(15, enemyTwoVerticalPosition, RED);
-  matrix.drawPixel(11, enemyThreeVerticalPosition, RED);
-  matrix.drawPixel(1, enemyFourVerticalPosition, RED);
-  matrix.drawPixel(enemyFiveHorizontalPosition, 14, RED);
-}
-
 void DrawKeys() {
   if (!keyOneCollected)
-    matrix.drawPixel(7, 5, BLUE);
+    matrix.drawPixel(8, 5, BLUE);
 
   if (!keyTwoCollected)
     matrix.drawPixel(18, 13, BLUE);
@@ -228,109 +228,16 @@ void DrawDoor() {
   matrix.fillRect(17, 16, 2, 3, YELLOW);
 }
 
-void DrawLevelOne() {
-  matrix.drawBitmap(0, 0, levelOne, 20, 20, GREEN);
-}
+void DrawLevel() {
+  uint16_t readValue;
 
-void UpdateEnemys() {
-  UpdateEnemyOne();
-  UpdateEnemyTwo();
-  UpdateEnemyThree();
-  UpdateEnemyFour();
-  UpdateEnemyFive();
-}
+  for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 20; j++) {
+      readValue = pgm_read_word(&levelOneData[j][i]);
 
-void UpdateEnemyOne() {
-  if (enemyOneCurrentStepCount != enemyOneStepCount) {
-    enemyOneCurrentStepCount = enemyOneCurrentStepCount + 1;
-    if (enemyOneDir == -1)
-      enemyOneHorizontalPosition = enemyOneHorizontalPosition - 1;
-    else
-      enemyOneHorizontalPosition = enemyOneHorizontalPosition + 1;
-  } else {
-    enemyOneCurrentStepCount = 0;
-
-    if (enemyOneDir == -1)
-      enemyOneDir = 1;
-    else
-      enemyOneDir = -1;
+      if (readValue == 1) {
+        matrix.drawPixel(i, j, GREEN);
+      }
+    }
   }
-
-  enemies[0][0] = enemyOneHorizontalPosition;
-}
-
-void UpdateEnemyTwo() {
-  if (enemyTwoCurrentStepCount != enemyTwoStepCount) {
-    enemyTwoCurrentStepCount = enemyTwoCurrentStepCount + 1;
-    if (enemyTwoDir == -1)
-      enemyTwoVerticalPosition = enemyTwoVerticalPosition - 1;
-    else
-      enemyTwoVerticalPosition = enemyTwoVerticalPosition + 1;
-  } else {
-    enemyTwoCurrentStepCount = 0;
-
-    if (enemyTwoDir == -1)
-      enemyTwoDir = 1;
-    else
-      enemyTwoDir = -1;
-  }
-
-  enemies[1][1] = enemyTwoVerticalPosition;
-}
-
-void UpdateEnemyThree() {
-  if (enemyThreeCurrentStepCount != enemyThreeStepCount) {
-    enemyThreeCurrentStepCount = enemyThreeCurrentStepCount + 1;
-    if (enemyThreeDir == -1)
-      enemyThreeVerticalPosition = enemyThreeVerticalPosition - 1;
-    else
-      enemyThreeVerticalPosition = enemyThreeVerticalPosition + 1;
-  } else {
-    enemyThreeCurrentStepCount = 0;
-
-    if (enemyThreeDir == -1)
-      enemyThreeDir = 1;
-    else
-      enemyThreeDir = -1;
-  }
-
-  enemies[2][1] = enemyThreeVerticalPosition;
-}
-
-void UpdateEnemyFour() {
-  if (enemyFourCurrentStepCount != enemyFourStepCount) {
-    enemyFourCurrentStepCount = enemyFourCurrentStepCount + 1;
-    if (enemyFourDir == -1)
-      enemyFourVerticalPosition = enemyFourVerticalPosition - 1;
-    else
-      enemyFourVerticalPosition = enemyFourVerticalPosition + 1;
-  } else {
-    enemyFourCurrentStepCount = 0;
-
-    if (enemyFourDir == -1)
-      enemyFourDir = 1;
-    else
-      enemyFourDir = -1;
-  }
-
-  enemies[3][1] = enemyFourVerticalPosition;
-}
-
-void UpdateEnemyFive() {
-  if (enemyFiveCurrentStepCount != enemyFiveStepCount) {
-    enemyFiveCurrentStepCount = enemyFiveCurrentStepCount + 1;
-    if (enemyFiveDir == -1)
-      enemyFiveHorizontalPosition = enemyFiveHorizontalPosition - 1;
-    else
-      enemyFiveHorizontalPosition = enemyFiveHorizontalPosition + 1;
-  } else {
-    enemyFiveCurrentStepCount = 0;
-
-    if (enemyFiveDir == -1)
-      enemyFiveDir = 1;
-    else
-      enemyFiveDir = -1;
-  }
-
-  enemies[4][0] = enemyFiveHorizontalPosition;
 }
